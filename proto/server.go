102bda976a06ef5a5aabd41069155ba8985367f1
YaqubRahman <YaqubRahman999@gmail.com>
2026-08-02 11:21:36 +0100

    feat: Get and Delete func in server.go

--- proto/server.go
+++ proto/server.go
@@ -11,3 +11,13 @@ func (s *server) Set(ctx context.Context, req *SetRequest) (*SetResponse, error)
 	s.store[req.Key] = req.Value
 	return &SetResponse{Success: true}, nil
 }
+
+func (s *server) Get(ctx context.Context, req *GetRequest) (*GetResponse, error){
+	value := s.store[req.Key]
+	return &GetResponse{Value: value}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *DeleteRequest) (*DeleteResponse, error){
+	delete(s.store, req.Key)
+	return &DeleteResponse{Success: true}, nil
+}
